7733309a153da12a42cb23a4a2e97a36608f2246
Spy <Spyngunga@gmail.com>
2023-10-22 04:29:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(r){
+  // Write your code here*/
+  
+  if (r <=400) {
+    return 'This one is on me!';
+  }
+  else if(r>2500) {
+    return 'No can do.';
+  }
+  else if(r>2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  }
+  
+  function ternaryCheckCity(city) {
+    // Write your code here!
+    return city === "NYC"? 'Ok, sounds good.':'No go.';
+  }
+  function switchOnCharmFromTip(tip) {
+    // Write your code here!
+    switch (tip) {
+      case "generous":
+        return "Thank you so much."
+        
+      case "not as generous":
+        return "Thank you."
+  
+      default:
+        return "Bye."
+        
+    }
+  }
